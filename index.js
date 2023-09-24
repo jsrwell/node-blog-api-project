110410e4432203fc2423caf9d7fb09290d0ffb4b
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-09-24 13:26:52 -0300

    Project Completed

--- index.js
+++ index.js
@@ -34,22 +34,63 @@ let posts = [
 
 let lastId = 3;
 
-// Middleware
-app.use(bodyParser.json());
+// ================== MIDDLEWARES ==================
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-//Write your code here//
+// ================== ROUTES ==================
+// GET all
+app.get("/posts", (req, res) => {
+  console.log(posts);
 
-//CHALLENGE 1: GET All posts
+  res.json(posts);
+});
 
-//CHALLENGE 2: GET a specific post by id
+// GET one
+app.get("/posts/:id", (req, res) => {
+  const post = posts.find((post) => post.id === parseInt(req.params.id));
+  if (!post) return res.status(404).json({ message: "Post not found" });
 
-//CHALLENGE 3: POST a new post
+  res.json(post);
+});
 
-//CHALLENGE 4: PATCH a post when you just want to update one parameter
+// CREATE one
+app.post("/posts", (req, res) => {
+  const newId = lastId += 1;
+  const post = {
+    id: newId,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    date: new Date(),
+  };
+  lastId = newId;
+  posts.push(post);
 
-//CHALLENGE 5: DELETE a specific post by providing the post id.
+  res.status(201).json(post);
+});
 
+// PATCH one
+app.patch("/posts/:id", (req, res) => {
+  const post = posts.find((p) => p.id === parseInt(req.params.id));
+  if (!post) return res.status(404).json({ message: "Post not found" });
+  if (req.body.title) post.title = req.body.title;
+  if (req.body.content) post.content = req.body.content;
+  if (req.body.author) post.author = req.body.author;
+
+  res.json(post);
+});
+
+// DELETE one (array splice logic)
+app.delete("/posts/:id", (req, res) => {
+  const index = posts.findIndex((p) => p.id === parseInt(req.params.id));
+  if (index === -1) return res.status(404).json({ message: "Post not found" });
+  posts.splice(index, 1);
+
+  res.json({ message: "Post deleted" });
+});
+
+// ================== LISTEN ==================
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
 });
